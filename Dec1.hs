841921b40069752b3cbc44f65d6609ad0781a89d
Hedquist <vilhelm.hedquist@hotmail.com>
2021-12-02 14:13:51 +0100

    day1 done

---
+++ Dec1.hs
@@ -0,0 +1,19 @@
+import System.FilePath
+
+test :: IO ()
+test = putStr "Hello World\n"
+
+depthIncreaseAmount :: FilePath -> IO Int 
+depthIncreaseAmount f = 
+    do file <- readFile f
+       let linedInts = map read $ lines file :: [Int]
+       return $ dea $ windows linedInts
+       
+dea :: [Int] -> Int
+dea [_]      = 0
+dea (x:y:zs) | x < y     = 1 + (dea $ y:zs)
+             | otherwise = dea $ y:zs
+
+windows :: [Int] -> [Int]
+windows (x:y:z:as) = [(x + y + z)] ++ windows (y:z:as)
+windows _          = []
